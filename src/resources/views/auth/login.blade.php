d31f8c834c6d6755b19f1fdfcc4275ae04292117
dbymmt <dbymmt@dbtstdomoutno01.com>
2024-02-18 21:56:53 +0900

    240218_in_progress_front_view

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register_login.css') }}">
+@endsection
+
+@section('content')
+
+@endsection
---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/thanks_done.css') }}">
+@endsection
+
+@section('content')
+
+@endsection
