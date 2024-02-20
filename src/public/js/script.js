ff2b39ec4629f9a20b1deaef93f51098f3c30787
dbymmt <dbymmt@dbtstdomoutno01.com>
2024-02-20 11:59:46 +0900

    2402201159_in_progress_fv

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     console.log('unko');
 
+    // 戻るボタン
+    // const back = document.getElementById('btn--back');
+    // back.addEventListener('click', (e) => { history.back(); return false; });
+
 });
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -14,14 +14,38 @@
   @yield('css')
 </head>
     <body>
-        <header>
-            <h1>
-                <a href="#">Rese</a>
-            </h1>
+        <header class="header">
+            <div class="header__logo">
+                <h1 class="header__logo">
+                    <a href="#">Rese</a>
+                </h1>
+            </div>
+            @if(request()->path() === "/")
+            <div class="header__search-menu">
+                <select name="area">
+                    <option value="">All Areas</option>
+                    <option value="1">北海道</option>
+                </select>
+                <select name="genre">
+                    <option value="">All Genres</option>
+                    <option value="1">寿司</option>
+                </select>
+                <input type="text" name="keyword" placeholder="Input restaurant name">
+            </div>
+            @endif
         </header>
+
+        {{-- ホームメニュー --}}
+        <nav class="header__nav">
+            <ul class="header__menu">
+                <li class="header__list"><a href="/">Home</a></li>
+                <li class="header__list"><a href="/register">Registration</a></li>
+                <li class="header__list"><a href="/login">Login</a></li>
+            </ul>
+        </nav>
+        <main>
+            @yield('content')
+        </main>
     </body>
-    <main>
-        @yield('content')
-    </main>
 
 </html?
